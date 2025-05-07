1482989221074d5ba32a8104e219d33ff272a500
Veer1578 <sweetyatulshah@gmail.com>
2025-05-07 09:43:08 +0530

    hw

---
+++ hw.py
@@ -0,0 +1,9 @@
+import math
+
+a = math.sin(0.5)
+b = math.cos(0.5)
+c = math.tan(0.5)
+
+print(f'The value of sin(0.5) is {a}')
+print(f'The value of cos(0.5) is {b}')
+print(f'The value of tan(0.5) is {c}')
